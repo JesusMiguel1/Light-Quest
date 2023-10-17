cef47ce8f0c446610acdb8ef4cf82cd679bb609d
JesusMiguel1 <114179269+JesusMiguel1@users.noreply.github.com>
2023-10-17 01:35:41 +0100

    Shooting mechanics

--- Assets/Scripts/Player Scripts/Player.cs
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -1,18 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Liminal.SDK.Core;
+using Liminal.SDK.VR;
+using Liminal.SDK.VR.Input;
+using Liminal.SDK.VR.Avatars;
+
 
 public class Player : MonoBehaviour
 {
+    private Transform enemies;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Getting avatar reference
+        var avatar = VRAvatar.Active;
+        if (avatar == null)
+        {
+            return;
+        }
+        GetInputs();
+
+        Debug.Log("Checking avatar... " + avatar);
+    }
+
+    //Initialize inputs
+    void GetInputs()
+    {
+        var rightInput = GetInput(VRInputDeviceHand.Right);
+        var leftInput = GetInput(VRInputDeviceHand.Left);
+
+        // Input Examples
+        if (rightInput != null)
+        {
+            if (rightInput.GetButtonDown(VRButton.Back))
+                Debug.Log("Back button pressed");
+
+
+
+        }
+
+        if (leftInput != null)
+        {
+            if (leftInput.GetButtonDown(VRButton.Back))
+                Debug.Log("Back button pressed");
+
+            if (leftInput.GetButtonDown(VRButton.One))
+                Debug.Log("Trigger button pressed");
+        }
+
+    }
+
+    private IVRInputDevice GetInput(VRInputDeviceHand hand)
+    {
+        var device = VRDevice.Device;
+        Debug.Log("Checking hands..." + hand);
+        return hand == VRInputDeviceHand.Left ? device.SecondaryInputDevice : device.PrimaryInputDevice;
+
     }
 }
---
+++ Assets/Scripts/Weapons/ShootingScript.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Liminal.SDK.VR.Input;
+using Liminal.SDK.VR;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    [SerializeField] Transform bulletSpawner;
+    RaycastHit hit;
+    float distance = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckIfEnemy();
+        Fire();
+
+    }
+
+    //Raycast to find enemy to shoot at them
+    void CheckIfEnemy()
+    {
+        //Ray ray;
+        float speed = 10f;
+
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward) * speed;
+        if (Physics.Raycast(transform.position, forward, out hit, distance, LayerMask.GetMask("Enemy")))
+        {
+            Debug.DrawRay(transform.position, forward, Color.green);
+            //Debug.Log("Hitting something... " + hit.point);
+        }
+    }
+
+    private IVRInputDevice GetInput(VRInputDeviceHand hand)
+    {
+        var device = VRDevice.Device;
+        //Debug.Log("Checking hands..." + hand);
+        return hand == VRInputDeviceHand.Left ? device.SecondaryInputDevice : device.PrimaryInputDevice;
+
+    }
+
+    private void Fire()
+    {
+        var rightHandInput = GetInput(VRInputDeviceHand.Right);
+        var leftHandInput = GetInput(VRInputDeviceHand.Left);
+
+        if (rightHandInput.GetButtonDown(VRButton.One))
+        {
+            Debug.Log("Lets start shooting");
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                GameObject bullet = BulletsPoolManager.Instance.GetBullet();
+                bullet.transform.position = transform.position;
+                bullet.transform.rotation = transform.rotation;
+            }
+        }
+
+    }
+}

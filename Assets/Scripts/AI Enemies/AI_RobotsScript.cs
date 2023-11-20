1c753a15527ac69c55a2a2a7bf69d08986942cc5
JesusMiguel1 <114179269+JesusMiguel1@users.noreply.github.com>
2023-11-20 16:35:10 +0000

    Better wave of enemies first part

--- Assets/Scripts/AI Enemies/AI_RobotsScript.cs
+++ Assets/Scripts/AI Enemies/AI_RobotsScript.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Jobs;
 using UnityEngine;
+using UnityEngine.Jobs;
 
-namespace punk_vs_robots
+namespace object_pool
 {
     public class AI_RobotsScript : MonoBehaviour
     {
@@ -30,10 +32,10 @@ namespace punk_vs_robots
             rb = this.GetComponent<Rigidbody>();
             audioSource = GetComponent<AudioSource>();
 
-            //if (player == null)
-            //{
-            //    //player = GameObject.FindGameObjectWithTag("Player").transform;
-            //}
+            if (player == null)
+            {
+                player = GameObject.FindGameObjectWithTag("Player").transform;
+            }
             audioManager = GetComponent<AudioManager>();
 
             // Initialize patrol direction
@@ -47,7 +49,16 @@ namespace punk_vs_robots
                 audioSource.volume = 0.05f;
                 audioSource.PlayOneShot(policeAudioClips);
             }
-            
+            if (!ifMovingToPlayer)
+            {
+                float randomX = Random.Range(0f, 20f);
+                float randomY = Random.Range(0f, 5f);
+                float randomZ = Random.Range(20f, 0f);
+
+                Vector3 newPosition = new Vector3(randomX, randomY, randomZ);
+                transform.Translate(newPosition * moveSpeed * Time.deltaTime);
+            }
+
 
             StartCoroutine(MoveToPlayer());
 
@@ -66,8 +77,12 @@ namespace punk_vs_robots
             {
                 moveSpeed = Random.Range(10f, 25f);
                 // Rotate towards the player
-                Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+                //Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+                //transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+                Vector3 direction = player.position - transform.position;
+                float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+                Quaternion rotation = Quaternion.Euler( 0f, angle, 0f);
+                transform.rotation = rotation;  
 
                 // Move towards the player
                 movement = moveDirection.normalized;
@@ -93,7 +108,7 @@ namespace punk_vs_robots
         void OnCollisionEnter(Collision other)
 
         {
-            if (other.gameObject.name == "Player")
+            if (other.gameObject.name == "Player(Clone)")
             {
                 //Debug.Log($"<b>Collision </b> <color=red> <b>{other.gameObject.name}</b> </color>");
                 gameObject.SetActive(false);
@@ -113,9 +128,110 @@ namespace punk_vs_robots
 
         void ColorfullExplosion()
         {
-            explosion = DestructiblePoolManager.Instance.GetPieces();
+            explosion = DestructiblePoolManager.Instance.GetExplosionPieces();
             explosion.transform.position = transform.position;
 
         }
-    }
+        //    public float moveSpeed = 5f;
+        //    private float rotationSpeed = 20;
+        //    private float detectPlayerRadius = 0.1f;
+        //    public bool ifMovingToPlayer = false;
+        //    Vector3 movement;
+        //    [SerializeField] Transform player; // Reference to the player's transform
+        //    Rigidbody rb;
+
+        //    GameObject wayPointPrefab;
+        //    GameObject explosion;
+
+        //    public AudioClip audioClips;
+        //    public AudioClip policeAudioClips;
+        //    AudioSource audioSource;
+
+        //    private AudioManager audioManager;
+
+
+        //    void Start()
+        //    {
+        //        moveSpeed = UnityEngine.Random.Range(20f, 30f);
+        //        rb = this.GetComponent<Rigidbody>();
+        //        audioSource = GetComponent<AudioSource>();
+
+        //        //if (player == null)
+        //        //{
+        //        //    //player = GameObject.FindGameObjectWithTag("Player").transform;
+        //        //}
+        //        audioManager = GetComponent<AudioManager>();
+
+        //        // Initialize patrol direction
+        //    }
+
+        //    void Update()
+        //    {
+        //        StartCoroutine(MoveToPlayer());
+
+        //        if (audioSource != null && policeAudioClips != null)
+        //        {
+        //            audioSource.volume = 0.03f;
+        //            audioSource.PlayOneShot(policeAudioClips);
+        //        }
+        //    }
+
+        //    IEnumerator MoveToPlayer()
+        //    {
+        //        float randomTime = Random.Range(0.1f, 4.1f);
+        //        yield return new WaitForSeconds(randomTime);
+
+        //        ifMovingToPlayer = true;
+
+        //        TransformAccessArray transforms = new TransformAccessArray(1);
+        //        transforms.Add(transform);
+
+        //        JobHandle jobHandle;
+
+        //        try
+        //        {
+        //            MoveToPlayerJob job = new MoveToPlayerJob
+        //            {
+        //                deltaTime = Time.deltaTime,
+        //                detectPlayerRadius = detectPlayerRadius,
+        //                rotationSpeed = rotationSpeed,
+        //                playerPosition = player.position,
+        //                moveSpeed = moveSpeed // Set the moveSpeed parameter
+        //            };
+
+        //            jobHandle = job.Schedule(transforms);
+        //            jobHandle.Complete();
+        //        }
+        //        finally
+        //        {
+        //            transforms.Dispose();
+        //        }
+        //    }
+        //    #region
+        //    void OnCollisionEnter(Collision other)
+
+        //    {
+        //        if (other.gameObject.name == "Player")
+        //        {
+        //            //Debug.Log($"<b>Collision </b> <color=red> <b>{other.gameObject.name}</b> </color>");
+        //            gameObject.SetActive(false);
+        //            ColorfullExplosion();
+
+        //        }
+        //        if (other.gameObject.name == "Bullet(Clone)")
+        //        {
+        //            AudioManager.Instance.PlayOneShot(audioClips);
+        //            //// int clipsIndex = Random.Range(0, audioClips.Length);
+
+        //        }
+        //    }
+        //    #endregion
+        //    void ColorfullExplosion()
+        //    {
+        //        explosion = DestructiblePoolManager.Instance.GetExplosionPieces();
+        //        explosion.transform.position = transform.position;
+
+        //    }
+
+        }
     }
---
+++ Assets/Scripts/AI Enemies/New Scripts/EnemySpawner.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace object_pool
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        private int enemiesAmount = 3;
+        private int numberOfWaves = 5;
+        private float delayWave = 2f;
+        private GameObject enemy;
+
+        void Start()
+        {
+            StartCoroutine(SpawnEnemiesTrigger());
+        }
+
+        IEnumerator SpawnEnemiesTrigger()
+        {
+            float timeToSpawn = 1f;
+            float waveTimeToSpan = 3f;
+
+            for (int wave = 0; wave < numberOfWaves; wave++)
+            {
+                yield return StartCoroutine(SpawnWave(enemiesAmount, timeToSpawn));
+                enemiesAmount += 3;
+                yield return new WaitForSeconds(waveTimeToSpan * delayWave);
+            }
+        }
+
+        IEnumerator SpawnWave(int numEnemies, float spawnDelay)
+        {
+            for (int i = 0; i < numEnemies; i++)
+            {
+                enemy = DronesPoolManager.Instance.GetDrones();
+                enemy.transform.position = new Vector3(Random.Range(-50, 50), 1f, Random.Range(-50, 50));
+                enemy.transform.rotation = transform.rotation;
+                yield return new WaitForSeconds(spawnDelay);
+            }
+        }
+
+
+
+
+
+
+
+        ////Trying to spawn enemies on random amount
+        //private float m_interval = 3f;
+        //private float m_time = 0f;
+        //private int minAmount = 1;
+        //private int maxAmount = 5;
+        //private int maxAmountOfEnemies = 10;
+        //public GameObject drone;
+
+
+
+
+        //void Update()
+        //{
+        //    if (!drone.activeInHierarchy)
+        //    {
+        //        //Increasing timer 
+        //        m_time += Time.deltaTime;
+
+        //        if (m_time >= m_interval)
+        //        {
+        //            //reset timer
+        //            m_time = 0f;
+
+        //            int amountOfEnemies = Random.Range(minAmount, maxAmount + 1);
+
+        //            for (int i = 0; i < amountOfEnemies; i++)
+        //            {
+        //                // Instantiate the enemy prefab at a random position
+        //                Vector3 spawnPosition = new Vector3(Random.Range(-50f, 50f), 5f, Random.Range(-60f, 60f));
+        //                //Instantiate(dronePrefab, spawnPosition, Quaternion.identity);
+        //                GameObject drone = DronesPoolManager.Instance.GetDrones();
+        //                drone.transform.position = spawnPosition;
+        //                //drone.transform.rotation = transform.rotation;
+        //            }
+        //        }
+        //    }
+        //}
+    }
+}

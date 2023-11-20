34fd90494393fed18ddc91ed3fcea519588541d0
JesusMiguel1 <114179269+JesusMiguel1@users.noreply.github.com>
2023-11-20 16:49:08 +0000

    Better wave second Part

--- Assets/Scripts/Objects pooll/DronesPoolManager.cs
+++ Assets/Scripts/Objects pooll/DronesPoolManager.cs
@@ -2,53 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DronesPoolManager : MonoBehaviour
+namespace object_pool
 {
-    private static DronesPoolManager instance;
-    public static DronesPoolManager Instance { get { return instance; } }
-
-
-    GameObject firstDrone;
-    GameObject secondDrone;
-
-    GameObject[] policeDronePrefab;
-    private List<GameObject> drones;
-
-    private int dronesAmount;
-    // Start is called before the first frame update
-    void Awake()
+    public class DronesPoolManager : MonoBehaviour
     {
-        dronesAmount = Random.Range(1, 10);
-        instance = this;
-        drones = new List<GameObject>(dronesAmount);
-        firstDrone = Resources.Load("D1", typeof(GameObject)) as GameObject;
-        secondDrone = Resources.Load("R1_Enemy", typeof(GameObject)) as GameObject;
+        private static DronesPoolManager instance;
+        public static DronesPoolManager Instance { get { return instance; } }
 
-        policeDronePrefab = new GameObject[] { secondDrone, firstDrone };
-        int dronesIndex = Random.Range(0, policeDronePrefab.Length);
-        for (int i = 0; i < dronesAmount; i++)
-        {
-            GameObject instantiate = Instantiate(policeDronePrefab[dronesIndex]);
-            instantiate.transform.SetParent(transform);
-            instantiate.SetActive(false);
-            drones.Add(instantiate);
-        }
-    }
 
-    public GameObject GetDrones()
-    {
-        foreach (GameObject drone in drones)
+        GameObject firstDrone;
+        GameObject secondDrone;
+
+        GameObject[] policeDronePrefab;
+        private List<GameObject> drones;
+
+        private int dronesAmount;
+        // Start is called before the first frame update
+        void Awake()
         {
-            if (!drone.activeInHierarchy)
+            dronesAmount = 40;
+            instance = this;
+            drones = new List<GameObject>(dronesAmount);
+            firstDrone = Resources.Load("slapper", typeof(GameObject)) as GameObject;
+            secondDrone = Resources.Load("R1_Enemy", typeof(GameObject)) as GameObject;
+
+            policeDronePrefab = new GameObject[] { firstDrone, secondDrone };
+            for (int i = 0; i < dronesAmount; i++)
             {
-                drone.SetActive(true);
-                return drone;
+                int dronesIndex = i % policeDronePrefab.Length;  // Use modulo to cycle through the prefabs
+
+                GameObject instantiate = Instantiate(policeDronePrefab[dronesIndex]);
+                instantiate.transform.SetParent(transform);
+                instantiate.SetActive(false);
+                drones.Add(instantiate);
             }
         }
-        int dronesIndex = Random.Range(0, policeDronePrefab.Length);
-        GameObject instantiate = Instantiate(policeDronePrefab[dronesIndex]);
-        instantiate.transform.SetParent(transform);
-        drones.Add(instantiate);
-        return instantiate;
+
+        public GameObject GetDrones()
+        {
+            foreach (GameObject drone in drones)
+            {
+                if (!drone.activeInHierarchy)
+                {
+                    drone.SetActive(true);
+                    return drone;
+                }
+            }
+            int dronesIndex = Random.Range(0, policeDronePrefab.Length);
+            GameObject instantiate = Instantiate(policeDronePrefab[dronesIndex]);
+            instantiate.transform.SetParent(transform);
+            drones.Add(instantiate);
+            return instantiate;
+        }
     }
 }
